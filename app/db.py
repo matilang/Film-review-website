c3dab3bf077462f780427de7a8dcc5396d0d17e9
Mateusz Łangowski <mateusz.langowski@otomin.pl>
2025-09-15 17:20:13 +0200

    database file changes

--- app/db.py
+++ app/db.py
@@ -4,7 +4,6 @@ from sqlmodel import create_engine
 sqlite_file_name = "database.db"
 sqlite_url = f"sqlite:///{sqlite_file_name}"
 
-connect_args = {"check_same_thread": False}
-engine = create_engine(sqlite_url, connect_args=connect_args)
+engine = create_engine(sqlite_url, connect_args={"check_same_thread": False})
 
     
