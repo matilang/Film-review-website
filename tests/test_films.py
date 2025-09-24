8dc58cc47d555775cfe65e4131bf94f04147488b
Mateusz Łangowski <mateusz.langowski@otomin.pl>
2025-09-24 19:34:45 +0200

    names changing 2

--- tests/test_films.py
+++ tests/test_films.py
@@ -11,43 +11,61 @@ def test_get_list_of_films(client, sample_list_of_films):
     
     for film in data:
         assert "title" in film
-        assert "author" in film
+        assert "director" in film
         assert "year" in film
 
-def test_get_list_of_films_with_limits(client, sample_list_of_films):    
-    response = client.get("/films/?offset=1")
+def test_get_list_of_films_with_limits(client, sample_list_of_films):   
+    # paginate 
+    response = client.get("/films/?limit=5&offset=0")
     assert response.status_code == 200
-    assert len(response.json()) == len(sample_list_of_films)-1
+    assert len(response.json()) == 5
     
-    response = client.get("/films/?author=Author C")
+    response = client.get("/films/?director=Director C")
     assert response.status_code == 200
-    assert len(response.json()) == len(sample_list_of_films)-2
+    assert len(response.json()) == 4
     
     response = client.get("/films/?year=2000")
     assert response.status_code == 200
-    assert len(response.json()) == len(sample_list_of_films)-2
+    assert len(response.json()) == 1
     
     response = client.get("/films/?limit=1")
     assert response.status_code == 200
-    assert len(response.json()) == len(sample_list_of_films)-2
+    assert len(response.json()) == 1
 
 def test_create_film(client):
     
-    film = {"title" : "Film A", "author" : "Author A", "year" : 2000}
+    film = {"title" : "Film A", "director" : "Director A", "year" : 2000}
     response = client.post("/films/", json=film)
+
     assert response.status_code == 201
-    
     assert response.json()['title'] == film['title']
-    assert response.json()['author'] == film['author']
+    assert response.json()['director'] == film['director']
     assert response.json()['year'] == film["year"]
     assert 'id' in response.json()
+    
+def test_create_film_with_missing_data(client):
+    # no title
+    response = client.post("/films/", json={"director" : "Director A", "year" : 2000})
+    assert response.status_code == 422
+    
+    # no director
+    response = client.post("/films/", json={"title" : "Film A", "year" : 2000})
+    assert response.status_code == 422
 
 def test_get_film_by_id(client, sample_list_of_films):
     film_id = sample_list_of_films[0]['id']
     response = client.get(f"/films/{film_id}")
+
     assert response.status_code == 200
     assert response.json()['id'] == film_id
     assert response.json()['title'] == sample_list_of_films[0]['title']
+    
+def test_get_film_by_wrong_id(client, sample_list_of_films):
+    film_list_length = len(sample_list_of_films)
+    film_id = sample_list_of_films[0]['id'] + film_list_length + 1
+    response = client.get(f"/films/{film_id}")
+
+    assert response.status_code == 404
     
 def test_update_film(client, sample_film):
     film_id = sample_film['id']
@@ -63,6 +81,14 @@ def test_update_film(client, sample_film):
     assert response.status_code == 200
     assert response.json()['title'] == new_film_title
     assert response.json()['id'] == film_id
+    
+def test_update_nonexistent_film(client, sample_film):
+    film_id = sample_film['id']
+    
+    new_film_title = "New Film"
+    wrong_id = film_id + 1
+    response = client.patch(f"/films/{wrong_id}", json={"title" : new_film_title})
+    assert response.status_code == 404
 
 def test_delete_film(client, sample_film):
     film_id = sample_film['id']
@@ -73,5 +99,12 @@ def test_delete_film(client, sample_film):
     assert response.status_code == 200
     
     response = client.get(f"films/{film_id}")
+    assert response.status_code == 404
+    
+def test_delete_nonexistent_film(client, sample_film):
+    film_id = sample_film['id']
+    
+    wrong_id = film_id + 1
+    response = client.delete(f"films/{wrong_id}")
     assert response.status_code == 404
     
--- tests/test_reviews.py
+++ tests/test_reviews.py
@@ -2,20 +2,38 @@ import pytest
 ## CRUD testing reviews
 
 
-def test_add_review(client, sample_film):
+def test_add_review(client, sample_film, sample_user_registration, sample_user_login):
     film_id = sample_film['id']    
     review = "Very nice film"
     rating = 2.3
 
     response = client.post(
         f"/films/{film_id}/reviews/", 
-        json={"film_id" : film_id, "review_text" : review, "rating" : rating}
+        json={"film_id" : film_id, "review_text" : review, "rating" : rating},
+        headers= {
+            "Authorization" : f"Bearer {sample_user_login['access_token']}"
+        }
         )
         
     assert response.status_code == 201
     assert response.json()['film_id'] == film_id
     assert response.json()['review_text'] == review
     assert response.json()['rating'] == rating
+    
+def test_add_review_with_wrong_rating(client, sample_film, sample_user_registration, sample_user_login):
+    film_id = sample_film['id']    
+    review = "Very nice film"
+    rating = 0.1
+
+    response = client.post(
+        f"/films/{film_id}/reviews/", 
+        json={"film_id" : film_id, "review_text" : review, "rating" : rating},
+        headers= {
+            "Authorization" : f"Bearer {sample_user_login['access_token']}"
+        }
+        )
+        
+    assert response.status_code == 422
     
 def test_get_all_reviews(client, sample_film, sample_list_of_reviews):
     film_id = sample_film['id']
@@ -26,6 +44,11 @@ def test_get_all_reviews(client, sample_film, sample_list_of_reviews):
     assert len(reviews) == len(sample_list_of_reviews)
     assert all(r['film_id'] == film_id for r in reviews)
 
+def test_get_nonexistent_reviews(client, sample_film):
+    film_id = sample_film['id']
+    response = client.get(f"/films/{film_id}/reviews/")
+    assert response.status_code == 404
+    
 def test_get_rating_average(client, sample_film, sample_list_of_reviews):
     film_id = sample_film['id']
     
