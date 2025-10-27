0bb40a594a45cb713ecb5f4e62cd85d1286d9940
Mateusz Łangowski <mateusz.langowski@otomin.pl>
2025-10-27 16:46:48 +0100

    adding middleware

--- app/main.py
+++ app/main.py
@@ -3,10 +3,24 @@ from sqlmodel import SQLModel
 from .routes import films, reviews
 from .db import engine
 from .auth import router as auth_router
+from fastapi.middleware.cors import CORSMiddleware
 
 SQLModel.metadata.create_all(engine)
 
 app = FastAPI()
+
+origins = [
+    "http://localhost:3000",
+    "http://127.0.0.1:3000"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(films.router)
 app.include_router(reviews.router)
